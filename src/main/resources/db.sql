1d483a66ec9e94eaaabfe875f05af9459430bb03
alexandra <aleksa.fom@gmail.com>
2016-10-08 03:31:28 +0200

    indexes

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -15,3 +15,10 @@ ALTER TABLE GRAPH ADD COLUMN girth INTEGER ;
 ALTER TABLE GRAPH ADD COLUMN primitive INTEGER ;
 ALTER TABLE GRAPH ADD COLUMN exp INTEGER ;
 ALTER TABLE GRAPH ADD COLUMN twoPartial INTEGER ;
+
+create index order_index on graph (vertex);
+create index edge_index on graph (edge);
+create index edgec_index on graph (EDGECON);
+create index c_index on graph (conn);
+create index rad_index on graph (radius);
+create index diam_index on graph (diametr);
