94af701371128a0ebf0c33a4be571be90211f44a
alexandra <aleksa.fom@gmail.com>
2016-10-08 17:35:46 +0200

    ui fixes

--- src/main/java/afomina/graphs/data/Graph.java
+++ src/main/java/afomina/graphs/data/Graph.java
@@ -55,7 +55,7 @@ public class Graph {
             int idx = 0;
             for (int i = 0; i < matrix.length - 1; i++) {
                 for (int j = i + 1; j < order; j++) {
-                    matrix[i][j] = matrix[j][i] = Short.parseShort(code2.charAt(idx++) + ""); //!TODO: not really sure about this
+                    matrix[i][j] = matrix[j][i] = Short.parseShort(code2.charAt(idx++) + "");
                     if (idx == code2.length()) {
                         break;
                     }
