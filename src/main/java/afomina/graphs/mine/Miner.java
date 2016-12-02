e45d0662158613f32ce62eb4c668921d1c3f9f83
Alexandra <aleksa.fom@gmail.com>
2016-12-02 18:27:27 +0300

    spmf

---
+++ src/main/java/afomina/graphs/mine/Miner.java
@@ -0,0 +1,10 @@
+package afomina.graphs.mine;
+
+import ca.pfv.spmf.algorithms.frequentpatterns.fpgrowth.AlgoFPGrowth;
+
+public class Miner {
+    public void mine() {
+        AlgoFPGrowth algo = new AlgoFPGrowth(); //http://www.philippe-fournier-viger.com/spmf/index.php?link=download.php
+//        algo.runAlgorithm() todo
+    }
+}
