b115d5b13d21acc4251d9a54be8592bee5309d82
Alexandra <aleksa.fom@gmail.com>
2016-12-14 12:39:45 +0400

    independence number

--- src/main/java/afomina/graphs/count/IndependenceNumber.java
+++ src/main/java/afomina/graphs/count/IndependenceNumber.java
@@ -2,13 +2,55 @@ package afomina.graphs.count;
 
 import afomina.graphs.data.Graph;
 
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class IndependenceNumber extends InvariantCounter<Integer> {
     @Override
     public Integer getInvariant(Graph g) {
-        int vectors = (int) Math.pow(2, g.getOrder());
-        for (int i = 0; i < vectors - 1; i++) {
-
+        int n = g.getOrder();
+        boolean[][] matrix = g.getMatrix();
+        int vectors = (int) Math.pow(2, n);
+        int maxIndependent = 0;
+        for (int i = 1; i < vectors - 1; i++) {
+            List<Integer> vector = vector(i, n);
+            if (checkIndependentSet(vector, matrix) && vector.size() > maxIndependent) {
+                maxIndependent = vector.size();
+            }
         }
-        return null; //TODO
+        return maxIndependent;
+    }
+
+    private boolean checkIndependentSet(List<Integer> set, boolean[][] g) {
+        for (int i = 0; i < set.size(); i++) {
+            Integer vertex1 = set.get(i);
+            for (int j = 0; j < set.size() && j != i; j++) {
+                Integer vertex2 = set.get(j);
+                if (g[vertex1][vertex2]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> vector(int i, int n) {
+        BigInteger bigInteger = new BigInteger(i + "");
+        String vector = bigInteger.toString(2);
+        if (vector.length() < n) {
+            int toAdd = n - vector.length();
+            for (int j = 0; j < toAdd; j++) {
+                vector = "0" + vector;
+            }
+        }
+
+        List<Integer> set = new ArrayList<>();
+        for (int j = 0; j < vector.length(); j++) {
+            if (vector.charAt(j) == '1') { // j-vertex is in set
+                set.add(j);
+            }
+        }
+        return set;
     }
 }
